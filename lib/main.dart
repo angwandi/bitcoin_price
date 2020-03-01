2f66d78f38cdd8485376e3859ff9e16547846fb1
Innocent Dema <31923567+angwandi@users.noreply.github.com>
2020-03-01 13:10:42 +0300

    Add device preview

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,21 @@
 import 'package:bitcoinprice/price_screen.dart';
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(
+      DevicePreview(
+        enabled: !kReleaseMode, //enable to see in action !kReleaseMode
+        builder: (context) => MyApp(),
+      ),
+    );
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      locale: DevicePreview.of(context).locale,
+      builder: DevicePreview.appBuilder,
       theme: ThemeData.dark().copyWith(
           primaryColor: Colors.lightBlue,
           scaffoldBackgroundColor: Colors.white),
